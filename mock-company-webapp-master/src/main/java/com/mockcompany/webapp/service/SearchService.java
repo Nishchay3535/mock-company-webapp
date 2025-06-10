e48627b58c9c9e15c861ceba046ef5cb1f8f6350
Nishchay3535 <163629371+Nishchay3535@users.noreply.github.com>
2025-06-10 15:10:14 +0530

    SearchService.java

--- mock-company-webapp-master/src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ mock-company-webapp-master/src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -19,15 +19,9 @@ public class SearchService {
         this.productItemRepository = productItemRepository;
     }
 
-    public Collection<ProductItem> search(String query) {
-        /*
-         * This is a simple implementation that loops over all the items and does the filtering in Java.
-         * A better implementation would do most if not all of the filtering in a query and then finalize or
-         * rank the results in Java.  From the SearchControllerSpec, we need to meet the following requirements:
-         *   1. query can be contained within either the name or description of the item
-         *   2. query string is treated as case-insensitive meaning Hi will match hi, hI, Hi, or HI
-         *   3. If the query is wrapped in quotes, only EXACT matches of name/description will be returned
-         */
+    public Collection<ProductItem> search(String query) { return Collections.emptyList(); // breaking test on purpose
+
+    
         Iterable<ProductItem> allItems = this.productItemRepository.findAll();
         List<ProductItem> itemList = new ArrayList<>();
 
